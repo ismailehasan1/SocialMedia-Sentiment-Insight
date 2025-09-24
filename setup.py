d0cfe232f3f4b7774b72f7ce9fa68675a8708ad0
ismailehasan1 <ismailehasanlio01@gmail.com>
2025-09-24 22:20:07 +0200

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Hasan',
+    author_email= 'ismailehasanlio1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
